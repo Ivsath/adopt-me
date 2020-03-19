f8335e948a3a705c49f3277d20422549191feb41
Irvin Reyes <reyes.yerp@gmail.com>
2020-03-19 16:45:48 -0600

    Create Details component and add reach router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router } from "@reach/router";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <React.StrictMode>
       <div>
         <h1>Adopt Me!</h1>
-        <SearchParams />
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     </React.StrictMode>
   );
