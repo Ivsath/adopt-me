b5c3d96feaf6ff5df0bc1055dd04116ad2c8824a
Irvin Reyes <reyes.yerp@gmail.com>
2020-03-19 15:42:13 -0600

    Update Pet component

--- src/Pet.js
+++ src/Pet.js
@@ -1,12 +1,20 @@
 import React from "react";
 
-const Pet = ({ name, animal, breed }) => {
+const Pet = ({ name, animal, breed, media, location, id }) => {
+  let hero = "http://placecorgi.com/300/300";
+  if (media.length) {
+    hero = media[0].small;
+  }
   return (
-    <div>
-      <h1>{name}</h1>
-      <h2>{animal}</h2>
-      <h2>{breed}</h2>
-    </div>
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+      </div>
+    </a>
   );
 };
 
